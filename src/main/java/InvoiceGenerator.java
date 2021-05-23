23477e0efe16c9f6db303101a6c0404119336185
Babakailash <nathkailash2020@gmail.com>
2021-05-24 01:42:15 +0530

    Calculate:Fare Given Distance and Time

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,5 +1,8 @@
 public class InvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("Welcome Cab Invoice Generator Program");
+    private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final int COST_PER_TIME = 1;
+
+    public double calculateFare(double distance, int time) {
+        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
     }
 }
